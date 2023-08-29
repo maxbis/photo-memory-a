a5741c78a7998c11130302516151e180cb659e5f
Max Bisschop <max@ffkijken.com>
2023-08-29 18:30:18 +0200

    v2

--- fetch_images.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['folder'])) {
-    $folder = $_GET['folder'];
-    $dir = "./images/$folder/";
-    $images = glob($dir . "*.{jpg,jpeg,png}", GLOB_BRACE);
-    $images = array_map('basename', $images); // Only keep file names
-    echo json_encode($images);
-}
